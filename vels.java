f2704c660d6bedb8d41e7d5b9ddcbc32338b5716
Raji-27 <109898341+Raji-27@users.noreply.github.com>
2023-01-30 10:57:46 +0530

    Add files via upload

---
+++ vels.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class vels{
+public static void main(String args[]){  
+int a=3;
+int b=0;
+int c;
+try{
+c=a/b; 
+}
+catch(ArithmeticException e){
+System.err.println("oops!exception occured!"); }
+} 
+}
+ 
